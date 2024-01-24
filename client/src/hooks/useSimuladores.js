078bad36f9fe44ea04414da30798bbbadac36a23
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2024-01-23 19:42:53 -0600

    Fixed domain names

--- client/src/hooks/useSimuladores.js
+++ client/src/hooks/useSimuladores.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const SendForm = (datos) => {
-	axios.post("http://127.0.0.1:5001/api/post/simulador", { data: datos });
+	axios.post("/api/post/simulador", { data: datos });
 };
 
 export const GetSimuladores = () => {
-	return axios.get("http://127.0.0.1:5001/api/get/simuladores");
+	return axios.get("/api/get/simuladores");
 };
