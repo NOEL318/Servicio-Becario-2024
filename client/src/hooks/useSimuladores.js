3a875aacd618957f827e8745924f9886096d2a5e
Noel Rincón <72935460+NOEL318@users.noreply.github.com>
2024-01-24 12:16:33 -0600

    Fixed IP

--- client/src/hooks/useSimuladores.js
+++ client/src/hooks/useSimuladores.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const SendForm = (datos) => {
-	return axios.post("http://127.0.0.1:5001/api/post/newsimulador", { data: datos });
+	return axios.post("/api/post/newsimulador", { data: datos });
 };
 
 export const GetSimuladores = () => {
-	return axios.get("http://127.0.0.1:5001/api/get/simuladores");
+	return axios.get("/api/get/simuladores");
 };
 
 export const GetSimulador = (_id) => {
-	return axios.post("http://127.0.0.1:5001/api/post/simulador", { _id });
+	return axios.post("/api/post/simulador", { _id });
 };
