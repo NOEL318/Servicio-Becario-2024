9599ad0ac66b93e1119464b0693232fb398934b2
NOEL318 <noelrinaya318@gmail.com>
2024-03-06 11:48:48 -0600

    AA

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.post("/api/loginwithoutpassword", async (req, res) => {
 
 app.post("/api/post/newsimulador", async (req, res) => {
 	var { datos } = req.body;
-	var insert = await mongodb.backup.insertOne(datos);
+	var insert = await mongodb.simuladores.insertOne(datos);
 	res.json(insert);
 });
 
@@ -63,15 +63,15 @@ app.post("/api/post/newsimulador", async (req, res) => {
 // });
 
 app.get("/api/get/simuladores", async (req, res) => {
-	var find = await mongodb.backup.find();
+	var find = await mongodb.simuladores.find();
 	const array = await find.toArray();
 	res.json(array);
 });
 
-// app.post("/api/post/backupsimuladores", async (req, res) => {
+// app.post("/api/post/simuladoressimuladores", async (req, res) => {
 // 	var find = await mongodb.simuladores.find();
 // 	const array = await find.toArray();
-// 	await mongodb.backup.insertMany(array);
+// 	await mongodb.simuladores.insertMany(array);
 // 	res.json(array);
 // });
 
@@ -95,7 +95,7 @@ app.post("/api/post/insertonlyone", async (req, res) => {
 		array = x;
 		first.numero_activo_fijo = activos;
 		first.cantidad = activos.length;
-		mongodb.backup.insertOne(first);
+		mongodb.simuladores.insertOne(first);
 		activos = [];
 	}
 	res.json(newarray);
@@ -109,7 +109,7 @@ app.post("/api/post/insertonlyone", async (req, res) => {
 
 app.post("/api/post/simulador", async (req, res) => {
 	const { _id } = req.body;
-	var find = await mongodb.backup.findOne({ _id: new ObjectId(_id) });
+	var find = await mongodb.simuladores.findOne({ _id: new ObjectId(_id) });
 	res.json(find);
 });
 
@@ -121,7 +121,7 @@ app.post("/api/post/simulador", async (req, res) => {
 
 app.post("/api/delete/simulador", async (req, res) => {
 	const { _id } = req.body;
-	var find = await mongodb.backup.deleteOne({ _id: new ObjectId(_id) });
+	var find = await mongodb.simuladores.deleteOne({ _id: new ObjectId(_id) });
 	res.json(find);
 });
 
@@ -135,7 +135,7 @@ app.post("/api/delete/simulador", async (req, res) => {
 
 app.post("/api/post/duplicate_simulador", async (req, res) => {
 	const _id = req.body;
-	var original = await mongodb.backup.findOne({ _id: new ObjectId(_id) });
+	var original = await mongodb.simuladores.findOne({ _id: new ObjectId(_id) });
 	// var copy = await mongodb.simuladores.insertOne({});
 
 	res.json(find);
